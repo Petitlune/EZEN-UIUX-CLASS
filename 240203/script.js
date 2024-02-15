cde1b11997c4020250f27a9790c132c692aeb52d
Petitlune <kmf11230@gmail.com>
2024-02-16 07:12:26 +0900

    240216

--- 240203/script.js
+++ 240203/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header");
 const famoon = document.querySelector("i");
-const btn = document.querySelector(".btn");
+const gnb = document.querySelector(".gnb");
 const button = document.querySelector(".trigger");
 
 window.addEventListener("scroll", () => {
@@ -18,5 +18,41 @@ window.addEventListener("scroll", () => {
   }
 });
 button.addEventListener("click", () => {
-  btn.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+
+$(".responsive").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
