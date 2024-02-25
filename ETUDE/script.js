87ae91967b9cf15482deba57bdf8e700a12fab59
Petitlune <kmf11230@gmail.com>
2024-02-26 00:10:18 +0900

    ETUDE-0226

---
+++ ETUDE/script.js
@@ -0,0 +1,58 @@
+//MainPage Product Click Event
+const mainProduct = document.querySelector(".product-hover");
+const productList = document.querySelector(".product-hover-menu");
+
+mainProduct.addEventListener("mouseenter", () => {
+  productList.classList.add("active-product");
+});
+productList.addEventListener("mouseleave", () => {
+  productList.classList.remove("active-product");
+});
+//MainPage Brand Click Event
+const mainbrand = document.querySelector(".brand-hover");
+const brandList = document.querySelector(".brand-hover-menu");
+console.log(mainbrand);
+
+mainbrand.addEventListener("mouseenter", () => {
+  brandList.classList.add("active-brand");
+});
+brandList.addEventListener("mouseleave", () => {
+  brandList.classList.remove("active-brand");
+});
+//MainPage Search Click Event
+const searchClickClose = document.querySelector(".search-click-icon i");
+const searchContent = document.querySelector(".search-click");
+const searchClickOpen = document.querySelector(".search-btn i ");
+console.log(searchContent);
+searchClickOpen.onclick = () => {
+  searchContent.classList.add("active-searchContent");
+};
+
+searchClickClose.onclick = () => {
+  searchContent.classList.remove("active-searchContent");
+};
+
+//MainPage tigger Click Event
+const mainTrigger = document.querySelector(".toggle-btn");
+const mainGnb = document.querySelector(".toggle-btn-click");
+
+mainTrigger.addEventListener("click", () => {
+  mainTrigger.classList.toggle("active");
+  if (mainGnb.style.display === "none" || mainGnb.style.display === "") {
+    mainGnb.style.display = "block";
+  } else {
+    mainGnb.style.display = "none";
+  }
+});
+//MainPage Store Brand
+const brandStore = document.querySelector(".brand-store-select");
+const brandStoreMenu = document.querySelector(".brand-store-select p");
+const brandStoreIcon = document.querySelector(".brand-store-select i");
+const brandStoreList = document.querySelector(".store-list");
+
+brandStore.addEventListener("click", () => {
+  brandStore.classList.toggle("active-store-list");
+  brandStoreList.classList.toggle("active-store-list");
+  brandStoreMenu.classList.toggle("active-store-list");
+  brandStoreIcon.classList.toggle("active-store-list");
+});
