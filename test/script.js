1e5ada7bfa1722f751b4463fdd726c46e3a25972
Petitlune <kmf11230@gmail.com>
2024-03-21 01:06:42 +0900

    240321

--- test/script.js
+++ test/script.js
@@ -3,71 +3,59 @@ const buttons = document.querySelectorAll(".btn span");
 const imgAll = document.querySelectorAll(".imgBox");
 console.log(imgAll);
 
-// const imgChange = () => {
-//   for (let i = 0; i < imgAll.length; i++) {
-//     if (buttons.length !== i) {
-//       imgAll[i].classList.add("active");
+const resetOtherButtons = (clickedIndex) => {
+  for (let i = 0; i < buttons.length; i++) {
+    if (i !== clickedIndex) {
+      buttons[i].classList.remove("active");
+      imgAll[i].classList.remove("active");
+      h1.classList.remove("active");
+      h1.innerText = "";
+    }
+  }
+};
 
-//       // h1.classList.add("active");
-//     }
+// const button = () => {
+//   for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", () => {
+//       resetOtherButtons(i);
+//     });
 //   }
 // };
 
-const reset = (i) => {
-  if (h1.innerText == String) {
-    imgAll[i - 1].classList.remove("active");
-    h1.classList.remove("active");
-    h1.innerText = "";
-    buttons[i - 1].classList.remove("active");
-  }
-};
+// button();
 
 const test = () => {
-  reset();
-  buttons[0].classList.remove("active");
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
-      if (i < buttons.length) {
-        imgAll[i].classList.add("active");
-        h1.classList.add("active");
-        h1.innerText = `Portfolio Slide#0${i + 1}`;
-        buttons[i].classList.add("active");
-      }
+      resetOtherButtons(i);
+      imgAll[i].classList.add("active");
+      h1.classList.add("active");
+      h1.innerText = `Portfolio Slide#0${i + 1}`;
+      buttons[i].classList.add("active");
     });
   }
 };
+
 test();
 
-// buttons.forEach((button) => {
-//   button.addEventListener("click", imgChange);
-// });
-
-// const one = () => {
-//   imgAll[0].classList.add("active");
-//   h1.classList.add("active");
-//   h1.innerText = "Portfolio Slide#01";
-//   buttons[0].classList.add("active");
-// };
-// const two = () => {
-//   imgAll[1].classList.add("active");
-//   h1.classList.add("active");
-//   h1.innerText = "Portfolio Slide#02";
-//   buttons[1].classList.add("active");
-// };
-// const three = () => {
-//   imgAll[2].classList.add("active");
-//   h1.classList.add("active");
-//   h1.innerText = "Portfolio Slide#03";
-//   buttons[2].classList.add("active");
+// const reset = () => {
+//   if (h1.innerText != "") {
+//     imgAll[i - 1].classList.remove("active");
+//     h1.classList.remove("active");
+//     h1.innerText = "";
+//     buttons[i - 1].classList.remove("active");
+//   }
 // };
 
-// buttons[0].addEventListener("click", () => {
-//   one();
+// const test = () => {
+//   for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", () => {
+//       imgAll[i].classList.add("active");
+//       h1.classList.add("active");
+//       h1.innerText = `Portfolio Slide #0${i + 1}`;
+//       buttons[i].classList.add("active");
+//     });
+//   }
+// };
 
-// });
-// buttons[1].addEventListener("click", () => {
-//   two();
-// });
-// buttons[2].addEventListener("click", () => {
-//   three();
-// });
+// test();
