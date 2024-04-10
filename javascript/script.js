94f11da021741142ecf01f9513ae55fba6ad5eb9
Petitlune <kmf11230@gmail.com>
2024-04-10 23:34:16 +0900

    240410

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-const bookTitle = document.querySelectorAll(".book-tit");
-const bookTitleHover = document.querySelectorAll("#book-tit-hover ");
-console.log(bookTitle[0].innerText);
-
-bookTitle.forEach((item, i) => {
-  bookTitleHover.forEach((it, idx) => {
-    it[idx].innerText += item[i].innerText;
-  });
-});
