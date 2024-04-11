6a57fe655b134361e6508e2d2637addaf1174ec2
Petitlune <kmf11230@gmail.com>
2024-04-11 23:55:39 +0900

    240411

--- javascript/Slide_Project/script.js
+++ javascript/Slide_Project/script.js
@@ -1,24 +1,18 @@
 const slides = document.querySelector("ul");
 const slide = slides.querySelectorAll("li");
 const slideCount = slide.length;
-const slidesWidth = 640;
+const slidesWidth = 750;
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
 let currentIdx = 0;
-
-// const updateWidth = () => {
-//   const li = document.querySelector(".slides li");
-//   const newslideCount = li.length;
-//   const newWidth = `${slidesWidth * newslideCount}px`;
-//   slides.style.width = newWidth;
-// };
-
+//초기값 세팅
 const setStart = () => {
   const startleft = -slidesWidth * slideCount;
   slides.style.transform = `translate(${startleft}px)`;
 };
 
+//앞뒤로 노드 복제
 const makeClone = () => {
   for (let i = 0; i < slideCount; i++) {
     const cloneSlide = slide[i].cloneNode(true);
@@ -30,15 +24,10 @@ const makeClone = () => {
     cloneSlide.classList.add("clone");
     slides.prepend(cloneSlide);
   }
-  setTimeout(() => {
-    slides.classList.add("animated");
-  }, 100);
   setStart();
-  // updateWidth();
 };
-
 makeClone();
-
+//무한 슬라이드 세팅
 const moveSlide = (num) => {
   slides.style.left = `${-num * slidesWidth}px`;
   currentIdx = num;
@@ -47,15 +36,22 @@ const moveSlide = (num) => {
       slides.classList.remove("animated");
       slides.style.left = "0px";
       currentIdx = 0;
-    }, 0);
-    setTimeout(() => {
-      slides.classList.add("animated");
-    }, 100);
+    }, 300);
   }
 };
+// const nextSlide = () => {
+//   for (let num = 1; num < slideCount; i++) {
+//     makeClone(num);
+//   }
+// };
+// let loopInterval = setInterval(() => {
+//   nextSlide();
+// }, 3000);
 nextBtn.addEventListener("click", () => {
+  slides.classList.add("animated");
   moveSlide(currentIdx + 1);
 });
 prevBtn.addEventListener("click", () => {
+  slides.classList.add("animated");
   moveSlide(currentIdx - 1);
 });
